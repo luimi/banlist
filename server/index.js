41b3ab5f0cefacb30fe8408e8ae444e22b87146e
lui2mi <luimi@live.com>
2021-08-15 08:29:46 -0500

    simplify server

--- server/index.js
+++ server/index.js
@@ -1,37 +1,18 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-var ParseDashboard = require('parse-dashboard');
+var app = express();
+require('dotenv').config()
+
 var api = new ParseServer({
-databaseURI: `${process.env.MONGODB}`,
+  databaseURI: `${process.env.MONGODB}`,
   cloud: './cloud/main.js',
   appId: process.env.APPID,
   masterKey: process.env.MASTERKEY,
   serverURL: `http://localhost:${process.env.PORT}/parse`
 });
 
-var options = { allowInsecureHTTP: true };
-
-var dashboard = new ParseDashboard({
-  "apps": [
-    {
-      "serverURL": process.env.URL,
-      "appId": process.env.APPID,
-      "masterKey": process.env.MASTERKEY,
-      "appName": "BanList"
-    }
-  ],
-  "users": [
-    {
-      "user":process.env.ADMIN,
-      "pass":process.env.PASSWORD
-    }
-  ]
-}, options);
-
-var app = express();
 app.use('/parse', api);
-app.use('/dashboard', dashboard);
 
-var httpServer = require('http').createServer(app);
-
-httpServer.listen(process.env.PORT);
+app.listen(process.env.PORT, () => {
+  console.log('Banlist ready');
+});
